fb5f7baee644220eae60a4d88deedc11f86ee564
Jamie Wilson <jamie@jamiewilson.io>
2015-05-06 11:11:00 -0500

    removing test pages, adding pushstate

--- static/scripts/community.js
+++ static/scripts/community.js
@@ -15,6 +15,7 @@ $(document).ready(function(){
         trigger.html("<a class='important flipIn' href='http://eepurl.com/bgi3M1' target='_blank'>That’s it for now. Get Notified of updates!</a>");
       } else {
         container.append(response);
+        history.pushState(null, null, url);
       }
     })
     // Advance page number variable
