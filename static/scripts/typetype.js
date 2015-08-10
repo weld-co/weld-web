870fc5e6be4e8b3858ac06982752b660885aa879
Jamie Wilson <jamie@jamiewilson.io>
2015-08-10 15:12:15 -0400

    refining css, changes to digital messaging

--- static/scripts/typetype.js
+++ static/scripts/typetype.js
@@ -4,22 +4,27 @@ $(document).ready(function() {
   // Pull content from data attr
   var content = $elem.attr("data-typetype-text");
 
+  function setCookie() {
+    setTimeout(function() {
+      $.cookie('hasSeenTyping', 'true', { expires: 60 });
+    }, 500)
+  };
+
+  setCookie();
+
   // If already seen, don't animate and show CTA
   if ($.cookie('hasSeenTyping') === 'true') {
     // Fill Element with content & fade in
     $elem.text(content);
     $('.main-cta').toggleClass('opacity-0 opacity-1');
-    
   } else {
-    $elem.typetype( content, { 
-        e: 0.01, // error rate
-        t: 50, // typing rate
-        callback: function () {
-          $.cookie('hasSeenTyping', 'true', { expires: 7 });
-          $('.main-cta').toggleClass('opacity-0 opacity-1');
-        }
+    $elem.typetype(content, {
+      e: 0.01, // error rate
+      t: 50, // typing rate
+      callback: function () {
+        $('.main-cta').toggleClass('opacity-0 opacity-1');
       }
-    )
+    })
   };
   // Used for dev puposes to delete cookie
   $('.logo').click(function() {
