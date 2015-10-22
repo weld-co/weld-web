a52fb79c237183d4d5d4081260191e0957b62a41
Jamie Wilson <jamie@jamiewilson.io>
2015-10-22 11:50:38 -0500

    assets for new homepage

--- static/scripts/search.js
+++ static/scripts/search.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
-   $(document).on('tipue_search_started', function() {
-      $('.search-form').addClass('active');
-   });
-
-   $('#tipue_search_input').tipuesearch({
-      highlightEveryTerm: true,
-      descriptiveWords: 50
-   });
+  $('#tipue_search_input').tipuesearch({
+    highlightEveryTerm: true,
+    descriptiveWords: 50
+  });
 });
